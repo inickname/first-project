7c965edefcec6159e1bb1acac1330db84d55e6fc
inickname <185136557+inickname@users.noreply.github.com>
2026-07-13 23:21:55 +0300

    Add hw_4_conditional_statements_and_loops_in_java

---
+++ src/main/java/hw_4_conditional_statements_and_loops_in_java/Main.java
@@ -0,0 +1,4 @@
+package hw_4_conditional_statements_and_loops_in__java;
+
+public class Main {
+}
---
+++ src/main/java/hw_4_conditional_statements_and_loops_in_java/solvers/IfElseTaskSolver.java
@@ -0,0 +1,7 @@
+package hw_4_conditional_statements_and_loops_in__java.solvers;
+
+public class IfElseTaskSolver {
+    public static void main(String[] args) {
+
+    }
+}
