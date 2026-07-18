b14af9d3540b97f556305f11b8f10e5fc3ba3784
inickname <185136557+inickname@users.noreply.github.com>
2026-07-18 22:57:06 +0300

    Add hw_4_conditional_statements_and_loops_in_java

--- src/main/java/hw_4_conditional_statements_and_loops_in_java/solvers/SwitchTaskSolver.java
+++ src/main/java/hw_4_conditional_statements_and_loops_in_java/solvers/SwitchTaskSolver.java
@@ -8,7 +8,15 @@ public class SwitchTaskSolver {
         // System.out.println(dayOfWeek());
 
         // 2. Стоимость билета по дню недели
+        // System.out.println(ticketPrice());
 
+        // 3. Перевод числовых оценок в буквенные (A–F)
+        // System.out.println(checkMark());
+
+        // 4. Обработка текстовых команд
+        // System.out.println(processingTextCommands());
+
+        // 5. Простой калькулятор с использованием switch
     }
 
     // 1. Вывод дня недели по номеру
@@ -49,5 +57,86 @@ public class SwitchTaskSolver {
     }
 
     // 2. Стоимость билета по дню недели
+    public static String ticketPrice() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите день недели: ");
+        int dayOfWeek = scanner.nextInt();
+        String ticketPrice = "";
+
+        switch (dayOfWeek) {
+            case 1, 2, 3, 4, 5:
+                ticketPrice = "300 рублей";
+                break;
+            case 6, 7:
+                ticketPrice = "450 рублей";
+                break;
+            default:
+                ticketPrice = "Unknown";
+        }
+
+        return ticketPrice;
+    }
+
+    // 3. Перевод числовых оценок в буквенные (A–F)
+    public static String checkMark() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите число: ");
+        int score = scanner.nextInt();
+        String mark = "";
+
+        switch (score / 10) {
+            case 9, 10:
+                mark = "A";
+                break;
+            case 8:
+                mark = "B";
+                break;
+            case 7:
+                mark = "C";
+                break;
+            case 6:
+                mark = "D";
+                break;
+            case 5, 4, 3, 2, 1, 0:
+                mark = "F";
+                break;
+            default:
+                mark = "Unknown";
+        }
+
+        return mark;
+    }
+
+    // 4. Обработка текстовых команд
+    public static String processingTextCommands() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите значение: ");
+        String command = scanner.nextLine();
+        String result = "";
+
+        switch (command) {
+            case "start":
+                result = "Система запущена";
+                break;
+            case "stop":
+                result = "Система остановлена";
+                break;
+            case "restart":
+                result = "Система перезапущена";
+                break;
+            case "status":
+                result = "Состояние системы";
+                break;
+            default:
+                result = "Ошибка ввода";
+        }
+
+        return result;
+    }
+
+    // 5. Простой калькулятор с использованием switch
 
 }
