407e092ac91adfeae8a7fa49fd891844863f5057
inickname <185136557+inickname@users.noreply.github.com>
2026-07-19 12:42:27 +0300

    Add hw_4_conditional_statements_and_loops_in_java

---
+++ src/main/java/hw_4_conditional_statements_and_loops_in_java/solvers/ForTaskSolver.java
@@ -0,0 +1,11 @@
+package hw_4_conditional_statements_and_loops_in_java.solvers;
+
+public class ForTaskSolver {
+    public static void main(String[] args) {
+        // 1. Вывод чисел от 1 до 100, делящихся на 3
+
+    }
+
+    // 1. Вывод чисел от 1 до 100, делящихся на 3
+
+}
