393bf6a3cf959ecf5b83ea6ed788f535a29b6634
inickname <185136557+inickname@users.noreply.github.com>
2026-07-20 21:07:44 +0300

    Add hw_4_conditional_statements_and_loops_in_java

---
+++ src/main/java/hw_4_conditional_statements_and_loops_in_java/solvers/WhileTaskSolver.java
@@ -0,0 +1,11 @@
+package hw_4_conditional_statements_and_loops_in_java.solvers;
+
+public class WhileTaskSolver {
+    public static void main(String[] args) {
+        // 1. Вычисление факториала с помощью while
+
+    }
+
+    // 1. Вычисление факториала с помощью while
+
+}
