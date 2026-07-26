c66d32e926070bc14f6a945d4d752b55d4034bfc
inickname <185136557+inickname@users.noreply.github.com>
2026-07-26 13:59:19 +0300

    Add hw_4_conditional_statements_and_loops_in_java

--- src/main/java/hw_4_conditional_statements_and_loops_in_java/solvers/DoWhileTaskSolver.java
+++ src/main/java/hw_4_conditional_statements_and_loops_in_java/solvers/DoWhileTaskSolver.java
@@ -1,11 +1,21 @@
 package hw_4_conditional_statements_and_loops_in_java.solvers;
 
+import java.util.Scanner;
+
 public class DoWhileTaskSolver {
     public static void main(String[] args) {
         // 1. Запрос положительного числа
-
+        guessNumber();
     }
 
     // 1. Запрос положительного числа
+    public static void guessNumber() {
+        Scanner scanner = new Scanner(System.in);
+        int number;
 
+        do {
+            System.out.print("Введите число: ");
+            number = scanner.nextInt();
+        } while (number <= 0);
+    }
 }
